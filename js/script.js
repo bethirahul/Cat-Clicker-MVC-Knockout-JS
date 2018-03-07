3681ff1c55ef8321ac808858f8f37fab998752b8
Rahul Bethi <rahulbethi@hotmail.com>
2018-03-07 01:35:40 -0800

    Set defer attribute to defer for script to wait till DOM is finished loading
    jQuery is removed as its not necessary

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
-function main()
-{
+//function main()
+//{
     //===== D A T A ============================================================
 
     var cats_info = {
@@ -50,6 +50,7 @@ function main()
         {
             vm.current_cat = ko.observable(this);
             console.log("Clicked cat link: " + vm.current_cat().name());
+            console.log(vm.current_cat().src());
         }
 
         vm.register_click = function()
@@ -60,127 +61,10 @@ function main()
     }
 
     ko.applyBindings(new ViewModel());
-
-    //--------------------------------------------------------------------------
-
-    /*var controller = {
-
-        display_cat_index : -1,
-
-        init : function()
-        {
-            model.init();
-            view_1.init();
-            view_2.init();
-        },
-
-        get_cats : function()
-        {
-            return model.cats;
-        },
-
-        get_cat : function(index)
-        {
-            return model.cats[index];
-        },
-
-        display_cat: function(index)
-        {
-            if(index != this.display_cat_index)
-            {
-                view_2.render_cat(model.cats[index]);
-                this.display_cat_index = index;
-                /**/ /*console.log("Clicked link: " + this.display_cat_index);
-            }
-        },
-
-        register_click: function()
-        {
-            /**/ /*console.log("Clicked photo: " + controller.display_cat_index);
-            model.cats[this.display_cat_index].clicks++;
-            view_2.render_clicks(model.cats[this.display_cat_index].clicks);
-        }
-    };*/
+//}
 
 
-    //===== V I E W - 1 ========================================================
-
-    /*var view_1 = {
-
-        init : function()
-        {
-            this.render_catLinks(controller.get_cats())
-        },
-
-        render_catLinks : function(cats)
-        {
-            for(var i=0; i<cats.length; i++)
-            {
-                var id = 'cat_link_' + i;
-                var list_element = '<li id="' + id + '">'
-                    + cats[i].name
-                    + '</p>';
-                $("#cat-links").append(list_element);
-                $("#" + id).click(
-                    (
-                        function(index)
-                        {
-                            return(
-                                function()
-                                {
-                                    controller.display_cat(index);
-                                }
-                            );
-                        }
-                    )(i)
-                );
-            }
-        }
-        
-    }
-
-
-    //===== V I E W - 2 ========================================================
-
-    var view_2 = {
-
-        init: function()
-        {
-            $("#cat-img").click(this.register_click);
-        },
-
-        register_click : function()
-        {
-            controller.register_click();
-        },
-
-        render_clicks: function(clicks)
-        {
-            $("#clicks").text("Number of clicks: " + clicks);
-        },
-
-        render_cat : function(cat)
-        {
-            var $_cat_img = $("#cat-img");
-
-            $("#cat-name").text(cat.name);
-            $_cat_img.attr("alt", cat.name);
-            $_cat_img.attr("src", cat.src);
-
-            this.render_clicks(cat.clicks);
-        }
-        
-    }
-
-
-    //===== S T A R T ==========================================================
-
-    controller.init();*/
-    //ViewModel();
-}
-
-
-$(document).ready(main);
+//$(document).ready(main);
 
 
 /*window.onload = function() {
